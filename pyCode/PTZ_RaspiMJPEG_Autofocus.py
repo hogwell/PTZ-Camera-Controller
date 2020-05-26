9413a1b80b9f55cf5a9e715f162e90af67d631da
hogwell <rickh-github@westernwares.com>
2020-05-26 10:01:29 -0600

    Removed unused methods

--- pyCode/PTZ_RaspiMJPEG_Autofocus.py
+++ pyCode/PTZ_RaspiMJPEG_Autofocus.py
@@ -54,7 +54,6 @@ class PTZ_RaspiMJPEG_Autofocus:
 
     def get_RaspiMJPEG_image(self):
 	s = self.img_filename
-#        if self.debug: print("get_RaspiMJPEG_image: %s" %(s))
         if self.debug: print("get_RaspiMJPEG_image()")
 	if self.last_RaspiMJPEG_image_time > 0:
 		dt = time.time() - self.last_RaspiMJPEG_image_time
@@ -107,7 +106,6 @@ class PTZ_RaspiMJPEG_Autofocus:
     def laplacian2(self,img):
         img_gray = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY) 
         img_sobel = cv2.Laplacian(img_gray,cv2.CV_64F).var()
-#        return img_sobel
         return cv2.mean(img_sobel)[0]
 
     def calculation(self):
@@ -131,7 +129,6 @@ class PTZ_RaspiMJPEG_Autofocus:
 	if step <= 0: step = 1
 	threshold = 1.0
 	max_dec_count = 1
-#	max_dec_count = 0
         focal_distance = best_focus
         if self.debug: print("\nstart_RaspiMJPEG_Autofocus() from %d to %d step %d" %(starting_point,self.MAX_FOCUS_VALUE,step))
 	try:
@@ -179,192 +176,5 @@ class PTZ_RaspiMJPEG_Autofocus:
 		time.sleep(.1)
 #	time.sleep(5)
         return best_focus,max_value
-
-    def focusing(self,step,threshold,max_dec_count):
-        self.value_buffer = []
-        max_index = self.focuser.get(Focuser.OPT_FOCUS)
-	if self.debug: print("focusing(): max_index=%d" % (max_index))
-        max_value = 0.0
-        last_value = -1
-        dec_count = 0
-        # step = 200
-        focal_distance = max_index
-#        self.focuser.set(Focuser.OPT_FOCUS,focal_distance)
-        while True:
-            #Adjust focus
-            self.focuser.set(Focuser.OPT_FOCUS,focal_distance)
-            #Take image and calculate image clarity
-            val = self.calculation()
-            if self.debug: print("calculation() = %d" %(val))
-            # print "calculation value:",val
-            val = self.filter(val) # get average of last 3 values
-            if self.debug: print("filter value = %d, focal_distance = %d"%(val,focal_distance))
-
-            #Find the maximum image clarity
-            if val > max_value:
-                max_index = focal_distance
-                max_value = val
-
-            #If the image clarity starts to decrease
-            if last_value - val > threshold :
-                if self.debug: print("clarity decrease(%d): last_value = %lf,current_value = %lf"%(dec_count,last_value,val))
-                dec_count += 1
-            elif last_value - val != 0:
-                dec_count = 0
-            #Image clarity is reduced by six consecutive frames
-            if dec_count > max_dec_count:
-                if self.debug: print("blurry, so exiting loop (%d)"%(max_dec_count))
-                break
-            last_value = val
-
-            #Increase the focal distance
-            focal_distance = self.focuser.get(Focuser.OPT_FOCUS)
-            focal_distance += step
-            if focal_distance > self.MAX_FOCUS_VALUE:
-                if self.debug: print("Hit maximum focus value (%d) at focal_distance=%d, so exiting loop"%(self.MAX_FOCUS_VALUE,focal_distance))
-                break
-
-	if self.debug:
-            print("focusing(): return max_index(focus)=%d,max_value(clarity)=%d" % (max_index,max_value))
-        return max_index,max_value
-
-    def CoarseAdjustment(self,st_point,ed_point):
-	focus_step = 100
-        images = []
-        index_list = []
-        eval_list = []
-        time_list = []
-	if self.debug: print("CoarseAdjustment(st_point=%d,ed_point=%d)" % (st_point,ed_point))
-#        while self.focuser.isBusy():
-#		time.sleep(.1)
-	if self.debug: print(" self.focuser.set(Focuser.OPT_FOCUS,%d)"%(st_point))
-        self.focuser.set(Focuser.OPT_FOCUS,st_point)
-#        rawCapture = PiRGBArray(self.camera) 
-#        self.camera.capture(rawCapture,format="bgr", use_video_port=True)
-#	if self.debug:
-#            print(" t = time.time()")
-	t = time.time()
-#	if self.debug:
-#            print(" time_list.append(%.3f)" % t)
-       	time_list.append(t)
-#	print "appended"
-#	except:
-#		print "except!!!"
-#		e0,e1,e2 = sys.exc_info()
-#		print e0+" - "+e1+" - "+e2
-#	print "appended2"
-	s = "/dev/shm/mjpeg/cam.jpg"
-	if self.debug: print(" Ready to read img_filename=%s" % s)
-
-	# Wait a little bit to make sure raspimjpeg
-	# has written a new image file.
-	time.sleep(self.MJPEG_DELAY)
-
-#       image = rawCapture.array
-	if self.debug: print(" image = cv2.imread(\"%s\")"%(s))
-	try:
-#		image = cv2.imread(IMG_FILENAME, cv2.IMREAD_COLOR)
-	    image = cv2.imread(s)
-	except:
-	    e = sys.exc_info()[0]
-	    print ("ERROR READING FIRST IMAGE: %s"%(str(e)))
-	    raise
-
-        images.append(image)
-#        rawCapture.truncate(0)
-
-	# Gather a set of images while focusing to the max focus value
-
-        # self.focuser.setFocusNoWait(self.focuser.end_point[int(self.focuser.getZoom()/1000)])
-#        self.focuser.set(Focuser.OPT_FOCUS,ed_point,0)
-#        while self.focuser.isBusy():
-#            self.camera.capture(rawCapture,format="bgr", use_video_port=True)
-        focus = self.focuser.get(Focuser.OPT_FOCUS) + focus_step
-	while focus <= ed_point:
-	    self.focuser.set(Focuser.OPT_FOCUS,focus)
-	    while self.focuser.isBusy():
-		pass
-
-            time_list.append(time.time())
-#            image = rawCapture.array
-	    # Wait a little bit to make sure raspimjpeg
-	    # has written a new image file.
-	    time.sleep(self.MJPEG_DELAY)
-#	    image = cv2.imread(IMG_FILENAME, cv2.IMREAD_COLOR)
-	    if self.debug:
-                print(" coarse focusing read image %d" % len(images))
-            image = cv2.imread(s)
-            images.append(image)
-#            rawCapture.truncate(0)
-            focus = self.focuser.get(Focuser.OPT_FOCUS) + focus_step
-
-        total_time = time_list[len(time_list) - 1] - time_list[0]
-        index_list = np.arange(len(images))
-        last_time = time_list[0]
-        if self.debug:
-            print("total images = %d"%(len(images)))
-            print("total time = %.3f"%(total_time))
-        for i in range(len(images)):
-            image = images.pop(0)
-            width = image.shape[1]
-            height = image.shape[0]
-            image = image[(height / 4):((height / 4) * 3),(width / 4):((width / 4) * 3)]
-            result = self.laplacian2(image)
-	    if self.debug: print("CoarseAdjustment laplacian2 %d=%.3f"%(i,result))
-            eval_list.append(result)
-        return eval_list,index_list,time_list
-
-    def startFocus(self):
-        begin = time.time()
-        self.focuser.reset(Focuser.OPT_FOCUS)
-        self.MAX_FOCUS_VALUE = self.focuser.end_point[int(self.focuser.get(Focuser.OPT_ZOOM)/1000)]
-        self.focuser.set(Focuser.OPT_FOCUS,self.focuser.starting_point[int(self.focuser.get(Focuser.OPT_ZOOM)/1000)])
-        if self.debug: print("startFocus: init time = %lf"%(time.time() - begin))
-        begin = time.time()
-        max_index,max_value = self.focusing(300,1,1)
-        # focuser.setFocus(0)
-        self.focuser.set(Focuser.OPT_FOCUS,max_index - 300 * (2) - 30)
-        # Careful adjustment
-        max_index,max_value = self.focusing(50,1,4)
-        self.focuser.set(Focuser.OPT_FOCUS,max_index - 30)
-        if self.debug: print("focusing time = %lf"%(time.time() - begin))
-        return max_index,max_value
-
-    def startFocus2(self):
-        begin = time.time()
-        self.focuser.reset(Focuser.OPT_FOCUS)
-        self.MAX_FOCUS_VALUE = self.focuser.end_point[int(self.focuser.get(Focuser.OPT_ZOOM)/1000)]
-        starting_point = self.focuser.starting_point[int(self.focuser.get(Focuser.OPT_ZOOM)/1000)]
-        if self.debug: print("startFocus2 init reset time = %lf"%(time.time() - begin))
-        begin = time.time()
-        if self.debug: print("startFocus2 CoarseAdjustment = focus from %d to %d"%(starting_point,self.MAX_FOCUS_VALUE))
-        eval_list,index_list,time_list = self.CoarseAdjustment(starting_point,self.MAX_FOCUS_VALUE)
-        max_index = np.argmax(eval_list)
-        total_time = time_list[len(time_list) - 1] - time_list[0]
-        max_time = time_list[max_index - 1] - time_list[0]
-        self.focuser.set(Focuser.OPT_FOCUS,int(((max_time - 0.0)/total_time)*(self.MAX_FOCUS_VALUE - starting_point)) + starting_point)
-        if self.debug: print("startFocus2 max_index=%d total_time=%lf max_time=%lf"%(max_index,total_time,max_time))
-        # Careful adjustment
-        if self.debug: print("startFocus2 fine focusing(50,1,4)")
-        max_index,max_value = self.focusing(50,1,4)
-        if self.debug: print("startFocus2 finished: max_index(focus)=%d max_value(clarity)=%lf"%(max_index,max_value))
-        self.focuser.set(Focuser.OPT_FOCUS,max_index - 30)
-        if self.debug: print("startFocus2 focusing time = %lf"%(time.time() - begin))
-        return max_index,max_value
-
-    def auxiliaryFocusing(self):
-        begin = time.time()
-        self.focuser.reset(Focuser.OPT_FOCUS)
-        self.MAX_FOCUS_VALUE = self.focuser.end_point[int(self.focuser.get(Focuser.OPT_ZOOM)/1000)]
-        starting_point = self.focuser.starting_point[int(self.focuser.get(Focuser.OPT_ZOOM)/1000)]
-        if self.debug: print("auxiliaryFocusing init time = %lf"%(time.time() - begin))
-        begin = time.time()
-        eval_list,index_list,time_list = self.CoarseAdjustment(starting_point,self.MAX_FOCUS_VALUE)
-        max_index = np.argmax(eval_list)
-        total_time = time_list[len(time_list) - 1] - time_list[0]
-        max_time = time_list[max_index] - time_list[0]
-        self.focuser.set(Focuser.OPT_FOCUS,int(((max_time - 0.0)/total_time)*(self.MAX_FOCUS_VALUE - starting_point)) + starting_point)
-        if self.debug: print("focusing time = %lf"%(time.time() - begin))
-        return max_index
 
 pass
